e6f54860e9ec5ffaab557cbd26dd60b0d74960c3
kes-phyl <danphylbert@gmail.com>
2022-12-16 12:16:30 +0000

    truthy and falsy test

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,20 @@
 const sum = require('./sum');
 
 
-it('Should add num1 with num 2', ()=>{
-    const result = sum(1,2);
-    expect(result).toBe(3);
+describe('testing matchers', ()=>{
+    it('Should add num1 with num 2', ()=>{
+        const result = sum(1,2);
+        expect(result).toBe(3);
+    })
+
+    it(
+        'should check whether it is a truthy value', ()=>{
+            const n = null;
+            expect(n).toBeFalsy();
+        }
+    )
+
 })
+
+
+
