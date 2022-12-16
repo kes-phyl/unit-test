437176e18d417990c3ebd0375dd1afcffe2efd1b
kes-phyl <danphylbert@gmail.com>
2022-12-16 12:22:04 +0000

    testing to add float

--- sum.test.js
+++ sum.test.js
@@ -13,6 +13,10 @@ describe('testing matchers', ()=>{
             expect(n).toBeFalsy();
         }
     )
+    it('adding float', ()=>{
+        const value = 0.1 + 0.2;
+        expect(value).toBeCloseTo(0.3);
+    })
 
 })
 
